ef6564f02f17ddf24884062c3dcb597c881e9242
AlanDAmario <vega93@live.it>
2024-05-21 11:01:00 +0200

    [UPD] add img and text

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 'use strict';
+console.log(Vue);
 
-const { createApp } = Vue
+const { createApp } = Vue;
 
-createApp({
+// createApp({
+//     data(){
+//         return {
+
+//         }
+//     }
+// }).mount('#app')
+const optionObj = {
     data() {
         return {
-            message: 
-        }
-    }
-}).mount('#app')
+            title: 'Hello world!',
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAedMAQCpaUc18fV8hjm7Ym-N9gJU5d4QV4g&s'
+        };
+    },
+};
+const app = createApp(optionObj);
+app.mount('#app')
